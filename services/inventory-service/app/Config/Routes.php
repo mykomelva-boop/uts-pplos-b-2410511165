edbeb3bdcc9df779507141348d497ce998ac9624
mykomelva-boop <mykomelva@gmail.com>
2026-05-01 16:56:45 +0700

    feat(inventory): register REST resource routes for all controllers

--- services/inventory-service/app/Config/Routes.php
+++ services/inventory-service/app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->resource('items', ['controller' => 'ItemController']);
+$routes->resource('suppliers', ['controller' => 'SupplierController']);
+$routes->resource('locations', ['controller' => 'LocationController']);
+$routes->resource('stock-movements', ['controller' => 'StockMovementController']);
